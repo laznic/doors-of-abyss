a8bc3a6612cda8bbaab915b42deaf188e1003ed2
Niklas L <niklas@demtrees.com>
2023-08-05 16:48:33 +0300

    Add initial setup to be worked on

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from '@/components/ui/button'
+
+function App() {
+  return <div className="App">
+    Hello
+    <Button>Clicky</Button>
+    </div>;
+}
+
+export default App;
