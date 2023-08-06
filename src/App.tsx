2a2d62fa8f010d34c1dc0af909e518f6bf4d4be7
Niklas L <niklas@demtrees.com>
2023-08-06 23:11:51 +0300

    Move context to higher level to have access to context hook earlier

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import { Button } from '@/components/ui/button'
-import { ChapterContextProvider } from "./modules/chapters/context/ChapterContext";
 
 function App() {
   return (
-    <ChapterContextProvider>
       <div className="App">
         Hello
         <Button>Clicky</Button>
       </div>
-    </ChapterContextProvider>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { ChapterContextProvider } from "./modules/chapters/context/ChapterContext";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement!); // eslint-disable-line
 
 root.render(
   <React.StrictMode>
-    <App />
+    <ChapterContextProvider>
+      <App />
+    </ChapterContextProvider>
   </React.StrictMode>
 );
