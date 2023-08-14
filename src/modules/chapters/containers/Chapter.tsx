4fdde07c8b77779534c814faebe723b882626199
Niklas L <niklas@demtrees.com>
2023-08-14 04:02:49 +0300

    Separate code to own component to improve maintainability a bit

--- src/modules/chapters/containers/Chapter.tsx
+++ src/modules/chapters/containers/Chapter.tsx
@@ -1,4 +1,3 @@
-import Canvas from "@/components/ui/canvas";
 import React, { useEffect, useRef, useState } from "react";
 import {
   ChapterContextType,
@@ -10,10 +9,9 @@ import useKeyPress from "../hooks/useKeyPress";
 import { animate, motion, usePresence } from "framer-motion";
 import ChapterText from "../components/ChapterText";
 import supabase from "@/lib/supabase";
-import { chunk } from "remeda";
-import { getRandomFromArray } from "@/lib/utils";
 import { usePlaySound } from "@/modules/sounds/hooks/usePlaySound";
 import { Button } from "@/components/ui/button";
+import ChapterActions from "../components/ChapterActions";
 
 interface ChapterProps {
   chapter: ChapterContextType["currentChapter"];
@@ -23,7 +21,6 @@ export default function Chapter({ chapter }: ChapterProps) {
   const { goToNextChapter } = useChapterContext();
   const [action] = chapter?.actions || [];
   const options = chapter?.options || [];
-  const notes = chapter?.notes || [];
   const canvasRef = useRef<HTMLCanvasElement>();
   const textAreaRef = useRef<HTMLTextAreaElement>();
   const hasOptions = options.length > 0;
@@ -89,110 +86,6 @@ export default function Chapter({ chapter }: ChapterProps) {
     goToNextChapter?.(action?.id);
   }
 
-  function renderActions() {
-    switch (action?.action_type) {
-      case "DRAW":
-        return (
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-          >
-            <Canvas ref={canvasRef} />
-            <p
-              className="text-muted -mb-20"
-              style={{ fontFamily: "PermanentMarker" }}
-            >
-              Draw something on the paper
-            </p>
-          </motion.div>
-        );
-      case "SHOW_PICTURE":
-        return (
-          <motion.img
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            className="w-[50dvw] mx-auto h-auto"
-            src={DOMPurify.sanitize(notes?.[0]?.image)}
-            alt="Action image"
-          />
-        );
-      case "NOTEBOOK_READ":
-        if (!Array.isArray(notes)) return null;
-
-        const [firstColumn, secondColumn] = chunk(notes, 12);
-
-        return (
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            className="top-0 w-full h-full absolute"
-          >
-            <div className="leading-[1.75dvw] top-[5dvw] absolute left-[7dvw] w-[15dvw] h-[20dvw] -skew-x-[28deg] rotate-12 text-left overflow-hidden whitespace-normal">
-              {firstColumn?.map((note) => (
-                <p
-                  key={note.id}
-                  className="text-slate-900 text-[1.75dvw]"
-                  style={{
-                    fontFamily: getRandomFromArray([
-                      "Caveat",
-                      "PermanentMarker",
-                      "Version01",
-                      "Version03",
-                    ]),
-                  }}
-                >
-                  {note.text}
-                </p>
-              ))}
-            </div>
-            <div className="leading-[1.75dvw] absolute left-[30dvw] top-[9dvw] w-[15dvw] h-[20dvw] -skew-x-[16deg] rotate-12 text-left overflow-hidden whitespace-normal">
-              {secondColumn?.map((note) => (
-                <p
-                  key={note.id}
-                  className="text-slate-900"
-                  style={{
-                    fontFamily: getRandomFromArray([
-                      "Caveat",
-                      "PermanentMarker",
-                      "Version01",
-                      "Version03",
-                    ]),
-                  }}
-                >
-                  {note.text}
-                </p>
-              ))}
-            </div>
-          </motion.div>
-        );
-      case "NOTEBOOK_WRITE":
-        return (
-          <motion.div className="leading-8 absolute left-80 w-72 h-80 -skew-x-[28deg] -top-64 rotate-12 text-left overflow-hidden whitespace-normal">
-            <textarea
-              ref={textAreaRef}
-              rows={10}
-              maxLength={30}
-              style={{
-                fontFamily: getRandomFromArray([
-                  "Caveat",
-                  "PermanentMarker",
-                  "Version01",
-                  "Version03",
-                ]),
-              }}
-              className="bg-transparent resize-none text-slate-900 placeholder-slate-800 outline-none"
-              placeholder="Type something"
-            />
-          </motion.div>
-        );
-      default:
-        return null;
-    }
-  }
-
   useKeyPress("Enter", handleContinue);
 
   function startAnimatingEllipse() {
@@ -224,9 +117,11 @@ export default function Chapter({ chapter }: ChapterProps) {
   return (
     <section className="grid mx-auto h-full w-full">
       <header className="relative w-full mt-32 lg:mt-12">
-        <div className="absolute w-1/2 h-[40dvw] min-h-[240px] top-0 left-0 right-0 mx-auto grid items-center text-center justify-center">
-          {renderActions()}
-        </div>
+        <ChapterActions
+          chapter={chapter}
+          canvasRef={canvasRef}
+          textAreaRef={textAreaRef}
+        />
 
         <svg
           version="1.1"
