e7fbe536024f99b3818e6953d476ce6948f0f721
Niklas L <niklas@demtrees.com>
2023-08-14 03:45:50 +0300

    Make logo appear with transition

--- src/modules/chapters/containers/Chapters.tsx
+++ src/modules/chapters/containers/Chapters.tsx
@@ -1,6 +1,6 @@
 import { Logotype } from "@/components/ui/logo";
 import SoundToggle from "@/modules/sounds/components/SoundToggle";
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import React from "react";
 import { useChapterContext } from "../context/ChapterContext";
 import Chapter from "./Chapter";
@@ -11,17 +11,22 @@ export default function Chapters() {
 
   return (
     <>
+      {!inIntro && (
+        <motion.div
+          initial={{ opacity: 0, scale: 0.95 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ delay: 3, duration: 2 }}
+          className="absolute left-6 top-6"
+        >
+          <Logotype className="w-48" />
+        </motion.div>
+      )}
+
       <div className="absolute right-4 top-4">
         <SoundToggle />
       </div>
 
       <AnimatePresence mode={"wait"}>
-        {!inIntro && (
-          <div className="absolute left-6 top-6">
-            <Logotype className="w-48" />
-          </div>
-        )}
-
         {inIntro ? (
           <Intro key={"intro"} />
         ) : (
